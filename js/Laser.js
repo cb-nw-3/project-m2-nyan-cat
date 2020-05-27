faf809a3e0748aa0d125e9335918ab87f17a0574
donnyesq <adonis@darmas.ca>
2020-05-27 18:58:57 -0400

    Changed title on html

--- js/Laser.js
+++ js/Laser.js
@@ -8,10 +8,10 @@ class Laser {
     this.laserBeam = document.createElement("div");
     this.laserBeam.style.width = "100px";
     this.laserBeam.style.height = "200px";
-    this.laserBeam.style.color = "red";
+    this.laserBeam.style.backgroundColor = "red";
     this.laserBeam.style.position = "absolute";
-    this.laserBeam.style.top = "200px";
-    this.laserBeam.style.left = "100px";
+    this.laserBeam.style.top = "20px";
+    this.laserBeam.style.left = "10px";
     this.laserBeam.style.zIndex = "111111";
   }
 
