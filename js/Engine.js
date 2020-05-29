7869027bf5979639ff5ec20d4a424370c7d7c7fe
Philppe <psalvail@assetscience.com>
2020-05-29 18:33:00 -0400

    Project nyan cat Completed

--- js/Engine.js
+++ js/Engine.js
@@ -1,6 +1,7 @@
 // The engine class will only be instantiated once. It contains all the logic
 // of the game relating to the interactions between the player and the
 // enemy and also relating to how our enemies are created and evolve over time
+
 class Engine {
   // The constructor has one parameter. It will refer to the DOM node that we will be adding everything to.
   // You need to provide the DOM node when you create an instance of the class
@@ -15,6 +16,9 @@ class Engine {
     // that contains instances of the Enemy class
     this.enemies = [];
     // We add the background image to the game
+    this.livesCounter = 3;
+    this.scoreCounter = 0;
+    this.levelCounter = 1;
     addBackground(this.root);
   }
 
@@ -22,7 +26,18 @@ class Engine {
   //  - Updates the enemy positions
   //  - Detects a collision between the player and any enemy
   //  - Removes enemies that are too low from the enemies array
+
   gameLoop = () => {
+    let player = this.player;
+    let root = this.root;
+    let bullet = null;
+    document.body.onkeyup = function (e) {
+      if (e.keyCode == 32) {
+        bullet = new Bullet(root, player.x);
+        console.log("bullet: ", bullet);
+      }
+    };
+    // console.log("bullet: ", bullet);
     // This code is to see how much time, in milliseconds, has elapsed since the last
     // time this method was called.
     // (new Date).getTime() evaluates to the number of milliseconds since January 1st, 1970 at midnight.
@@ -47,6 +62,7 @@ class Engine {
     });
 
     // We need to perform the addition of enemies until we have enough enemies.
+
     while (this.enemies.length < MAX_ENEMIES) {
       // We find the next available spot and, using this spot, we create an enemy.
       // We add this enemy to the enemies array
@@ -57,17 +73,68 @@ class Engine {
     // We check if the player is dead. If he is, we alert the user
     // and return from the method (Why is the return statement important?)
     if (this.isPlayerDead()) {
-      window.alert('Game over');
+      clearInterval(gameStart);
+      btnStart.style.display = "";
+      btnStart.innerHTML = "Play Again";
+      let gameOver = setInterval(function () {
+        btnStart.style.visibility =
+          btnStart.style.visibility == "hidden" ? "visible" : "hidden";
+      }, 500);
+      audio.pause();
+      audio.currentTime = 0;
+      audio = new Audio("./images/war.mp3");
+      audio.play();
+      clearTimeout(this.gameLoop);
       return;
     }
 
     // If the player is not dead, then we put a setTimeout to run the gameLoop in 20 milliseconds
     setTimeout(this.gameLoop, 20);
   };
-
   // This method is not implemented correctly, which is why
   // the burger never dies. In your exercises you will fix this method.
+
   isPlayerDead = () => {
-    return false;
+    let playerHeight = this.player.domElement.style.top.substr(
+      0,
+      this.player.domElement.style.top.length - 2
+    );
+    let playerPositionXmax = this.player.x + PLAYER_WIDTH;
+    let playerPositionXMin = this.player.x;
+    this.enemies.forEach((enemy) => {
+      if (
+        enemy.y + ENEMY_HEIGHT >= playerHeight &&
+        enemy.x >= playerPositionXMin &&
+        enemy.x < playerPositionXmax
+      ) {
+        enemy.y = 501;
+        this.livesCounter -= 1;
+        livesRemain.innerHTML = this.livesCounter;
+      }
+      if (enemy.y + ENEMY_HEIGHT > GAME_HEIGHT) {
+        enemy.y = 501;
+        scoreValue.innerHTML = this.scoreCounter += 10;
+        this.checkScore(this.scoreCounter);
+      }
+    });
+    return this.livesDepleted(this.livesCounter);
+  };
+  livesDepleted = (livesRemaining) => {
+    let isDead = false;
+    if (livesRemaining === 0) {
+      isDead = true;
+    }
+    return isDead;
+  };
+  checkScore = (totalScore) => {
+    if (totalScore % 200 === 0 && GAME_WIDTH < window.screen.width) {
+      this.livesCounter += 1;
+      this.levelCounter += 1;
+      MAX_ENEMIES += 1;
+      GAME_WIDTH += PLAYER_WIDTH;
+      bg.style.width = GAME_WIDTH + "px";
+      livesRemain.innerHTML = this.livesCounter;
+      levelValue.innerHTML = this.levelCounter;
+    }
   };
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,85 @@
 // We create an instance of the Engine class. Looking at our index.html,
 // we see that it has a div with an id of `"app"`
-const gameEngine = new Engine(document.getElementById('app'));
+let audio = new Audio("./images/GroundZero.mp3");
+let gameEngine = {};
+let isFirstGame = true;
+let btnStart = document.createElement("BUTTON");
+let startGame = document.getElementById("app");
+btnStart.innerHTML = "Click Here to Start";
+btnStart.id = "btnGame";
+btnStart.setAttribute(
+  "style",
+  "position: absolute; top: 50%; left:50%; font-size: 30px; color:white; background-color: transparent; border: none; transform: translate(-50%, -50%); outline: none;"
+);
+btnStart.addEventListener("click", function () {
+  audio.loop = true;
+  audio.play();
+  this.style.display = "none";
+  gameEngine.gameLoop();
+  if (btnStart.innerHTML === "Play Again") {
+    bg = null;
+    gameEngine = {};
+    isFirstGame = false;
+    this.style.display = "none";
+    audio.pause();
+    audio.currentTime = 0;
+    audio = new Audio("./images/GroundZero.mp3");
+    audio.play();
+    gameEngine = new Engine(startGame);
+    gameEngine.gameLoop();
+  }
+});
+let mainBackground = document.getElementById("app");
+mainBackground.setAttribute("style", "position: relative; width: fit-content;");
+mainBackground.appendChild(btnStart);
+
+let livesAndValue = document.createElement("div");
+livesAndValue.setAttribute(
+  "style",
+  "position: absolute; top: 2%; right:2%; font-size: 25px; color:white; background-color: transparent; display: inline;"
+);
+let livesLabel = document.createElement("span");
+livesLabel.innerHTML = "Lives: ";
+
+let livesRemain = document.createElement("span");
+livesRemain.innerHTML = 3;
+livesAndValue.appendChild(livesLabel);
+livesAndValue.appendChild(livesRemain);
+mainBackground.appendChild(livesAndValue);
+
+let scoreAndValue = document.createElement("div");
+scoreAndValue.setAttribute(
+  "style",
+  "position: absolute; top: 2%; left:2%; font-size: 25px; color:white; background-color: transparent; display: inline;"
+);
+let scoreLabel = document.createElement("span");
+scoreLabel.innerHTML = "Score: ";
+let scoreValue = document.createElement("span");
+scoreValue.innerHTML = 0;
+scoreAndValue.appendChild(scoreLabel);
+scoreAndValue.appendChild(scoreValue);
+mainBackground.appendChild(scoreAndValue);
+
+let levelAndValue = document.createElement("div");
+levelAndValue.setAttribute(
+  "style",
+  "position: absolute; top: 2%; left:50%; font-size: 25px; color:white; background-color: transparent; transform: translate(-50%); transparent; display: inline;"
+);
+let levelLabel = document.createElement("span");
+levelLabel.innerHTML = "Level: ";
+let levelValue = document.createElement("span");
+levelValue.innerHTML = 0;
+levelAndValue.appendChild(levelLabel);
+levelAndValue.appendChild(levelValue);
+mainBackground.appendChild(levelAndValue);
+
+let gameStart = setInterval(function () {
+  btnStart.style.visibility =
+    btnStart.style.visibility == "hidden" ? "visible" : "hidden";
+}, 500);
+if (isFirstGame) {
+  gameEngine = new Engine(startGame);
+}
 
 // keydownHandler is a variable that refers to a function. The function has one parameter
 // (does the parameter name matter?) which is called event. As we will see below, this function
@@ -9,19 +88,18 @@ const gameEngine = new Engine(document.getElementById('app'));
 const keydownHandler = (event) => {
   // event.code contains a string. The string represents which key was press. If the
   // key is left, then we call the moveLeft method of gameEngine.player (where is this method defined?)
-  if (event.code === 'ArrowLeft') {
+  if (event.code === "ArrowLeft") {
     gameEngine.player.moveLeft();
   }
 
   // If `event.code` is the string that represents a right arrow keypress,
   // then move our hamburger to the right
-  if (event.code === 'ArrowRight') {
+  if (event.code === "ArrowRight") {
     gameEngine.player.moveRight();
   }
 };
 
 // We add an event listener to document. document the ancestor of all DOM nodes in the DOM.
-document.addEventListener('keydown', keydownHandler);
+document.addEventListener("keydown", keydownHandler);
 
 // We call the gameLoop method to start the game
-gameEngine.gameLoop();
